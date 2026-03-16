6274e060408d6d6013210052c8224e517de4eed1
sebas-ortiz <sebastiaws@gmail.com>
2026-03-15 18:28:35 -0600

    Update 04_views.sql

--- sql/04_views.sql
+++ sql/04_views.sql
@@ -1,1 +1,23 @@
-
+CREATE OR ALTER VIEW dbo.Visualizar_Consulta
+AS
+SELECT DISTINCT
+    p.cedula,
+    p.nombre,
+    p.apellido1,
+    p.apellido2,
+    d.provincia,
+    d.canton,
+    d.distrito
+FROM dbo.Tbl_PersonaTSE AS p
+INNER JOIN dbo.Tbl_LugarVotacion AS lv
+    ON lv.cedula = p.cedula
+INNER JOIN dbo.Tbl_DistritoElectoral AS d
+    ON d.codigo_electoral = lv.codigo_electoral
+WHERE
+    UPPER(d.provincia) = 'CARTAGO'
+    AND EXISTS (
+        SELECT 1
+        FROM dbo.Telefonos_General AS t
+        WHERE t.Cedula = CAST(p.cedula AS VARCHAR(20))
+    );
+GO
